2dbb0bceaa3f5fd60f54effb18dda5b23b6ca7de
Zhang <hunter.1534@outlook.com>
2019-11-01 02:37:55 +0700

    CORS enabled

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const createError = require('http-errors');
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -25,6 +26,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(cors({
+  exposedHeaders: ['Authorization'],
+  optionsSuccessStatus: 200,
+}));
 
 // routes setup
 app.use('/', indexRouter);
